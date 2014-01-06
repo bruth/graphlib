1456ac888f2e158009b68141c321730e45f845d9
Byron Ruth <b@devel.io>
2014-01-05 20:12:54 -0500

    0.9.2 Beta

--- graphlib/__init__.py
+++ graphlib/__init__.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals, absolute_import
 __version_info__ = {
     'major': 0,
     'minor': 9,
-    'micro': 1,
-    'releaselevel': 'final',
+    'micro': 2,
+    'releaselevel': 'beta',
     'serial': 1
 }
 
